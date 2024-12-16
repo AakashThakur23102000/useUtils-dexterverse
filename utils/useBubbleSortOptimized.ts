2b9436c723f95ecc78587b3d901b12b3a508aa99
Aakash Thakur <aakash.thakur@sharajman.com>
2024-12-16 16:21:05 +0530

    Added reverse fucntionlaity in bubble sort.

--- utils/useBubbleSortOptimized.ts
+++ utils/useBubbleSortOptimized.ts
@@ -1,15 +1,30 @@
-export const useBubbleSortOptimized = (arr: (string | number)[]): (string | number)[] => {
-
-  for (var i = 0; i < arr.length - 1; i++) {
-    var noSwap = true;
-    for (var j = 0; j < arr.length - (1 + i); j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        noSwap = false;
+export const useBubbleSortOptimized = (arr: (string | number)[], reverse = false): (string | number)[] => {
+  if (reverse === false) {
+    for (var i = 0; i < arr.length - 1; i++) {
+      var noSwap = true;
+      for (var j = 0; j < arr.length - (1 + i); j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+          noSwap = false;
+        }
+      }
+      if (noSwap === true) {
+        break;
       }
     }
-    if (noSwap === true) {
-      break;
+
+  } else {
+    for (let i = 0; i < arr.length - 1; i++) {
+      let noSwap = true;
+      for (let j = 0; j < arr.length - (1 + i); j++) {
+        if (arr[j] < arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+          noSwap = false;
+        }
+      }
+      if (noSwap === true) {
+        break;
+      }
     }
   }
 
